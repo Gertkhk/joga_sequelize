aeff2cc0ae1393bb8009cddf301ccf48802a2707
Gertkhk <gert.sarv@khk.ee>
2023-06-05 11:33:50 +0300

    Created article table and added table data

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
+'use strict';
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize')
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-sequelize
-    .authenticate()
-    .then(() =>{
-        console.log('Connected to the database');
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js' &&
+            file.indexOf('.test.js') === -1
+        );
     })
-    .catch(err=> {
-        console.error('Unable to connect to the database:', err)
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
     });
 
-app.get ("/", (req,res) =>{
-    res.json({ message: "Welcome to sequelize application."});
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
-app.listen(3000, () =>{
-    console.log("Server is running on http://localhost:3000")
-})
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
